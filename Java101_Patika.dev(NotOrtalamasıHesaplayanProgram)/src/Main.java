3916c16408a2da64da16b6a1cee67206797ae7f6
Sinan Özer <106543738+SinanOzer45@users.noreply.github.com>
2022-12-31 01:20:16 +0300

    Add files via upload
    Basit şekilde hazırlanmış, kullanıcıdan gerekli bilgileri alıp ders not ortalaması hesabı yapabilen ve sınıfı geçip geçmediğini söyleyen program kodlarım.
    ( My program codes are prepared in a simple way, can take the necessary information from the user, calculate the course grade point average and say whether he/she passed the class or not)

---
+++ Java101_Patika.dev(NotOrtalamasıHesaplayanProgram)/src/Main.java
@@ -0,0 +1,46 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Değişkenlerimizi tanımlıyoruz (ders isimleri)
+        int matematik, fizik, kimya, biyoloji, tarih, cografya, muzik;
+
+          //Scanner sınıfını tanımlıyoruz // buradaki input nesne
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuzu Giriniz: ");
+        matematik = input.nextInt();
+
+        System.out.print("Fizik Notunuzu Giriniz: ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya Notunuzu Giriniz: ");
+        kimya = input.nextInt();
+
+        System.out.print("Biyoloji Notunuzu Giriniz: ");
+        biyoloji = input.nextInt();
+
+        System.out.print("Tarih Notunuzu Giriniz: ");
+        tarih = input.nextInt();
+
+        System.out.print("Coğrafya Notunuzu Giriniz: ");
+        cografya = input.nextInt();
+
+        System.out.print("Müzik Notunuzu Giriniz: ");
+        muzik = input.nextInt();
+
+        //ortalamayı hesaplayan kodumuzu yazıyoruz
+
+        int toplam = (matematik +fizik +kimya +biyoloji +tarih +cografya +muzik);
+        double ortalama = (toplam/7.0);
+        System.out.println("Not Ortalamanız: " + ortalama);
+
+        // Not Ortalaması 60 dan Küçük ise Kaldı, eğer 60 dan büyükse Geçti şeklinde koşulu yazdığımız kod
+
+        String kosul = ortalama<60 ? "Sınıfta Kaldı" : "Sınıfı Geçti";
+        System.out.println(kosul);
+    }
+}
