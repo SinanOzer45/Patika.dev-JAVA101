4dde2f3a010cf902a1641a7a46ccee8c22e9e196
Sinan Özer <106543738+SinanOzer45@users.noreply.github.com>
2023-01-16 11:58:56 +0300

    Add files via upload

---
+++ numberCalculator/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	int n;
+	int total=0;
+	Scanner input = new Scanner(System.in);
+	//////girilen tek sayı değerlerinin toplanması////
+	/*do {
+        System.out.println("Sayı Giriniz : ");
+	    n = input.nextInt();
+	    if (n % 2 == 1){
+	        total+= n;
+        }
+    }while (n>0);
+            System.out.println("Toplam : "+total);  */
+
+		//////2 ve 4 ün katları olan sayıları toplayıp, tek sayı girildiğinde duran program/////
+		do {
+			System.out.println("Sayı Giriniz : ");
+			n = input.nextInt();
+			if (n % 2 == 0 && n % 4 == 0){
+				total+= n;
+			}
+		}while (n % 2 == 0);
+		System.out.println("Toplam : "+ total);
+
+
+
+
+    }
+}
