e0fa6b3223de4644ebe7321d02ff90e7fa4e34e9
Sinan Özer <106543738+SinanOzer45@users.noreply.github.com>
2023-01-29 11:39:06 +0300

    triangle drawing with star

---
+++ triangleDrawingWithStar/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(" N sayısını giriniz : ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {     // kaç satır gezeceğimizi belirler. //
+
+            for (int j = 1; j <= (n - i); j++) {     // satır başından ne kadar boşluk bırakılması gerektiğini belirler. //
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {     // her satırda kaç adet yıldız olması gerektiğini belirler. //
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+       for (int i =(n-1);i >= 1; i--){               // kaç satır gezeceğimizi belirler. //
+
+           for (int j = 1;j <= (n-i); j++){           // satır başından ne kadar boşluk bırakılması gerektiğini belirler. //
+               System.out.print(" ");
+           }
+           for (int k = 1;k <= (2 * i) - 1; k++){       // her satırda kaç adet yıldız olması gerektiğini belirler. //
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+    }
+}
+
