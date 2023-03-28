38b8a51c59a3ec3dfd3151155ba0e16db4e25501
Sinan Özer <106543738+SinanOzer45@users.noreply.github.com>
2023-03-29 01:13:59 +0300

    Add files via upload

---
+++ divisorsNumber/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int divisorsNumber=0;
+
+        for (int i=1; i<=100; i++){
+            for (int j=1; j<=i; j++){
+                if(i % j ==0){
+                   divisorsNumber++;
+                }
+            }
+            if(divisorsNumber ==2){
+                System.out.print(" "+i);
+            }
+              divisorsNumber =0;
+        }
+        System.out.println(" asal sayılardır.");
+    }
+}
